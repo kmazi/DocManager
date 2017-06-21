6b6cda7cef9caea06c76b9c940cdc7e1527053fc
kmazi <ugochukwu.mazi@andela.com>
2017-06-21 08:53:27 +0100

    chore(routes): map document access route - Create routes to access and create document - Delivers #147402837

---
+++ server/controller/document.js
@@ -0,0 +1,30 @@
+/**
+ * function to create a document
+ * @param {object} req - an object that contains the request body
+ * @param {object} res - an object that contains the response body
+ * @return {null} it returns no value
+ */
+const createDocument = (req, res) => {
+
+};
+/**
+ * function to fetch all documents from the database
+ * @param {object} req - an object that contains the request body
+ * @param {object} res - an object that contains the response body
+ * @return {null} it returns no value
+ */
+const getAllDocuments = (req, res) => {
+
+};
+
+/**
+ * function to fetch a specific document from the database
+ * @param {object} req - an object that contains the request body
+ * @param {object} res - an object that contains the response body
+ * @return {null} it returns no value
+ */
+const findDocument = (req, res) => {
+
+};
+
+export { createDocument, getAllDocuments, findDocument };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,18 @@
 import path from 'path';
+import { createDocument,
+        getAllDocuments,
+        findDocument } from '../controller/document';
 
 const routes = (router) => {
-  // Setup a default catch-all route that sends back a welcome message in JSON format.
+  // setup path for serving static files
   const sourcePath = path.join(__dirname, '../../client/');
+  // route to create a new document
+  router.post('/documents', createDocument);
+  // route to get all documents
+  router.get('/documents', getAllDocuments);
+  // route to find a specific document
+  router.get('/documents/id', findDocument);
+
   router.get('/', (req, res) => res.sendFile(`${sourcePath}client.html`));
   router.get('*', (req, res) => {
     res.send('404 error has occured! The page you\'re searching for cannot be found');
