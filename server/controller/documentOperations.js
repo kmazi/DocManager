34c41e114b41d69bda61a2d579674fc9463e168a
kmazi <ugochukwu.mazi@andela.com>
2017-06-22 14:53:56 +0100

    chore(tests): test route - Test function that creates route - Delivers #147402837

--- server/controller/documentOperations.js
+++ server/controller/documentOperations.js
@@ -4,19 +4,27 @@ import index from '../models';
  * function to create a document
  * @param {object} req - an object that contains the request body
  * @param {object} res - an object that contains the response body
- * @return {null} it returns no value
+ * @return {boolean} it returns true if the document was created
+ * and false otherwise
  */
 const createDocument = (req, res) => {
   const document = index.Document;
-  document.findOrCreate({
-    where: { title: req.body.title },
-    defaults: {
-      body: req.body.body,
-      access: req.body.access
-    }
-  }).spread((docCreated, created) => {
-    res.status(200).send(created);
-  });
+  const title = req.body.title;
+  const body = req.body.body;
+  const access = req.body.access;
+  if (title === '' || body === '' || access === '') {
+    res.status(422).send('Validation error!');
+  } else {
+    document.findOrCreate({
+      where: { title },
+      defaults: {
+        body,
+        access
+      }
+    }).spread((docCreated, created) => {
+      res.status(200).send(created);
+    });
+  }
 };
 /**
  * function to fetch all documents from the database
--- spec/serverspec/documentOperationSpec.js
+++ spec/serverspec/documentOperationSpec.js
@@ -1,7 +1,21 @@
-import { createDocument } from '../../server/controller/documentOperations';
+import request from 'request';
 
 describe('Creating documents:', () => {
-  it('', () => {
-    
+  it('should set status code to 422 at when empty form is sent',
+  (done) => {
+    const endPoint = 'http://localhost:1844/document';
+    request.post(endPoint, (error, response) => {
+      expect(response.statusCode).toBe(422);
+      done();
+    });
+  });
+
+  it('should return true when document is successfully created',
+  (done) => {
+    const endPoint = 'http://localhost:1844/document';
+    request.get(endPoint, (error, response, body) => {
+      expect(body).toBe(true);
+      done();
+    });
   });
 });
