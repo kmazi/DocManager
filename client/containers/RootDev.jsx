c5459ac41ab855f416fe13be0c3e48cc54f231db
kmazi <ugochukwu.mazi@andela.com>
2017-07-21 11:09:04 +0100

    fix(deployment): Deployment error - Fix devtool error in production - Delivers #147402837

--- client/containers/RootDev.jsx
+++ client/containers/RootDev.jsx
@@ -5,7 +5,6 @@ import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
 import App from '../components/App';
-import DevTools from './DevTools';
 /**
  * The entry point of the redux application
  * @param {object} store - It contains the state of the application
@@ -18,7 +17,6 @@ export default function Root({ store, history }) {
         <ConnectedRouter history={history}>
           <Route path="/" component={App} />
         </ConnectedRouter>
-        <DevTools />
       </div>
     </Provider>
   );
--- client/store/configureStore.dev.js
+++ client/store/configureStore.dev.js
@@ -3,7 +3,6 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
-import DevTools from '../containers/DevTools';
 
 const history = createHistory();
 const middleware = routerMiddleware(history);
@@ -17,8 +16,7 @@ const configureStore = initialState => createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(thunkMiddleware, middleware),
-    DevTools.instrument()
+    applyMiddleware(thunkMiddleware, middleware)
   )
 );
 export { history, configureStore };
