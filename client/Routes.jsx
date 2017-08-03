ab18a3cc16ced92a1f925789a53bfa42f0871545
kmazi <ugochukwu.mazi@andela.com>
2017-08-03 21:39:22 +0100

    fix(security) Secure routes - Secure other pages aside the signup and signin pages -delivers #148201451

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Home from './components/presentation/Home';
 import UserPage from './components/UserPage';
+
+const PrivateRoute = ({ component: Component, redirectTo, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) => {
+        return localStorage.getItem('docmanagertoken') ? (
+          <Component {...routeProps} />
+      ) : (
+        <Redirect to={{
+          pathname: redirectTo,
+          state: { from: routeProps.location }
+        }}
+        />
+      );
+    }}
+    />
+  );
+};
 
 export default (
   <Switch>
     <Route exact path="/" component={Home} />
+    <PrivateRoute path="/user/documents" redirectTo="/" component={UserPage} />
     <Route path="/user/documents" component={UserPage} />
   </Switch>
 );
