602abf987054819a6b558a0f879b36e0a2f22ba3
kmazi <ugochukwu.mazi@andela.com>
2017-08-20 18:45:48 +0100

    fix(test) supertest - migrate test from jasmine to mocha - migrate from using request to supertest

---
+++ server/mocks/users.js
@@ -0,0 +1,40 @@
+const mockUsers = [{
+  userName: 'james',
+  email: 'james@gmail.com',
+  password: 'testing1',
+  roleId: 5,
+  isactive: true,
+}, {
+  userName: 'john',
+  email: 'john@gmail.com',
+  password: 'testing1',
+  roleId: 3,
+  isactive: true,
+}, {
+  userName: 'paul',
+  email: 'paul@gmail.com',
+  password: 'testing1',
+  roleId: 4,
+  isactive: true,
+}, {
+  userName: 'peterson',
+  email: 'peter@gmail.com',
+  password: 'testing1',
+  roleId: 3,
+  isactive: true,
+}, {
+  userName: 'prince',
+  email: 'prince@gmail.com',
+  password: 'testing1',
+  roleId: 3,
+  isactive: true,
+},
+{
+  userName: 'matthew',
+  email: 'matthew@gmail.com',
+  password: 'testing1',
+  roleId: 3,
+  isactive: true,
+}];
+
+export default mockUsers;
