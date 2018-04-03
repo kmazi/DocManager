3008452f8692853f06915c62132c3e42a1150b6f
kmazi <ugochukwu.mazi@andela.com>
2018-04-03 15:30:31 +0100

    chore(design): Redisign doc view 	- change the css on the doc view for mobile view

--- server/app.js
+++ server/app.js
@@ -2,21 +2,30 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import path from 'path';
-import dotenv from 'dotenv';
 import Webpack from 'webpack';
 import webpackdevmiddleware from 'webpack-dev-middleware';
 import webpackhotmiddleware from 'webpack-hot-middleware';
+
 import config from '../webpack.config';
 import routes from './routes/routes';
 // Set up the express app
 const app = express();
-const compiler = Webpack(config);
 const router = express.Router();
-dotenv.config();
+if (process.env.NODE_ENV === "development") {
+  const compiler = Webpack(config);
+  app.use(webpackdevmiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    noInfo: true,
+    historyApiFallback: true,
+    hot: true
+  }));
+
+  app.use(webpackhotmiddleware(compiler));
+}
 
 // set static path
 const sourcePath = express.static(path.join(__dirname, '../client/src/assets'));
-app.use('/', sourcePath);
+app.use('/static', sourcePath);
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -24,15 +33,6 @@ app.use(logger('dev'));
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(webpackdevmiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  noInfo: true,
-  historyApiFallback: true,
-  hot: true
-}));
-
-app.use(webpackhotmiddleware(compiler));
 
 app.use('/api/v1', router);
 // get all routes
